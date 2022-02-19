741eb0c00daa2dc5b55bffc7eaefe64dfaf12914
johanngerberding <johann.gerberding@gmail.com>
2022-02-19 11:39:35 +0100

    added argparser and lr-scheduler to training script

--- dqn/train.py
+++ dqn/train.py
@@ -1,20 +1,17 @@
-from functools import total_ordering
-import os
-from pickletools import optimize 
+import os 
+import argparse 
 import torch
-import copy
-import random
-import gym
+import json 
 import gym.spaces 
 from collections import deque, namedtuple
 import numpy as np 
+from datetime import date 
 from torch.utils.tensorboard import SummaryWriter
 
 from atari_wrappers import generate_env
 from dqn import DQN_Conv
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 Experience = namedtuple(
     "Experience", field_names=[
@@ -86,40 +83,76 @@ class Agent:
 
                   
         
-def main():    
-    # Hyperparameters 
-    eps_start = 1.0
-    eps_decay = 0.999985
-    eps_min = 0.02
-    replay_buffer_size = 10000
-    init_experiences = 10000
-    sync_target_network = 1000
-    batch_size = 32
-    learning_rate = 1e-4
-    reward_bound = 19.0
-    gamma = 0.99
+def main():  
+    parser = argparse.ArgumentParser()  
+    parser.add_argument("--env", type=str, 
+                        help="Name of the gym environment", 
+                        default="PongNoFrameskip-v4")
+    parser.add_argument("--gamma", type=float, 
+                        help="Gamma hyperparameter", 
+                        default=0.99)
+    parser.add_argument("--lr", type=float, 
+                        help="Learning Rate", 
+                        default=1e-4)
+    parser.add_argument("--batch_size", type=int, 
+                        help="Batch size to train Q-Network", 
+                        default=32)
+    parser.add_argument("--eps", type=float, 
+                        help="Epsilon starting value", 
+                        default=1e-4)
+    parser.add_argument("--eps_min", type=float, 
+                        help="Epsilon minimum value", 
+                        default=0.02)
+    parser.add_argument("--eps_decay", type=float, 
+                        help="Epsilon decay rate", 
+                        default=0.999985)
+    parser.add_argument("--replay_buffer", type=int, 
+                        help="Size of the replay buffer", 
+                        default=100000)
+    parser.add_argument("--min_exps", type=int, 
+                        help="Minimum number of experiences before training", 
+                        default=10000)
+    parser.add_argument("--sync", type=int, 
+                        help="Network synchronisation interval", 
+                        default=1000)
+    parser.add_argument("--reward_bound", type=float, 
+                        help="Bound for reward to be done", 
+                        default=21.0)
+    parser.add_argument("--out", type=str, 
+                        help="Path to output directory", default=None)
+    args = parser.parse_args()
     
-    # env_name = "PongDeterministic-v4"
-    env_name = "PongNoFrameskip-v4"
-    env = generate_env(env_name)
-    frame = env.reset()
+    if not args.out:
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        d = date.today().strftime("%d-%m-%Y")
+        outdir = os.path.join(curr_dir, args.env + "_" + d)
+    else: 
+        outdir = args.out 
     
-    print(type(frame))
-    print(frame.shape)
-    print(env.observation_space.shape)
-    print(env.action_space.n)
+    os.makedirs(outdir, exist_ok=False)
+    
+    # Save hyperparameters 
+    with open(os.path.join(outdir, "params.json"), 'wt') as f:
+        json.dump(vars(args), f, indent=4)
+    
+    env = generate_env(args.env)
+    _ = env.reset()
     
     Q_network = DQN_Conv(4, env.action_space.n).to(DEVICE)
     Q_tar_network = DQN_Conv(4, env.action_space.n).to(DEVICE)
     
-    replay_buffer = ReplayBuffer(replay_buffer_size)
+    replay_buffer = ReplayBuffer(args.replay_buffer)
     agent = Agent(env, replay_buffer)
     
-    writer = SummaryWriter(comment="-" + env_name)
+    writer = SummaryWriter(
+        log_dir=outdir,
+        comment="-" + args.env
+    )
     
-    eps = eps_start
+    eps = args.eps
     
-    optimizer = torch.optim.Adam(Q_network.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(Q_network.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
     
     total_rewards = []
     frame_idx = 0 
@@ -128,7 +161,7 @@ def main():
     while True: 
         # Sampling Phase
         frame_idx += 1 
-        eps = max(eps * eps_decay, eps_min)
+        eps = max(eps * args.eps_decay, args.eps_min)
         reward = agent.step(Q_network, eps)
         
         if reward is not None: 
@@ -141,26 +174,27 @@ def main():
             writer.add_scalar("reward", reward, frame_idx)
             
             if best_mean_reward is None or best_mean_reward < mean_reward:
-                torch.save(Q_network.state_dict(), env_name + "-best.pth")
+                torch.save(Q_network.state_dict(), 
+                           os.path.join(outdir, args.env + "-best.pth"))
                 best_mean_reward = mean_reward
 
                 if best_mean_reward is not None:
                     print(f"Best mean reward updated: {best_mean_reward}")
             
-            if mean_reward > reward_bound:
+            if mean_reward > args.reward_bound:
                 print(f"Solved in {frame_idx} frames.")
                 break
                 
-        if len(replay_buffer) < init_experiences:
+        if len(replay_buffer) < args.min_exps:
             continue 
         
-        if frame_idx % sync_target_network == 0:
+        if frame_idx % args.sync == 0:
             Q_tar_network.load_state_dict(Q_network.state_dict())
             print("Target Network updated.")
         
         # Training Phase
         optimizer.zero_grad()
-        batch = replay_buffer.sample(batch_size)
+        batch = replay_buffer.sample(args.batch_size)
         states, actions, rewards, dones, next_states = batch 
         
         states_t = torch.tensor(states).to(DEVICE)
@@ -175,11 +209,11 @@ def main():
         next_state_values[dones_t] = 0.0 
         next_state_values = next_state_values.detach() 
         
-        expected_state_action_values = next_state_values * gamma + rewards_t 
+        expected_state_action_values = next_state_values * args.gamma + rewards_t 
         loss_t = torch.nn.MSELoss()(state_action_values, expected_state_action_values)
         
         loss_t.backward()
-        optimizer.step() 
+        scheduler.step(loss_t) 
         
     writer.close()
     env.close()
